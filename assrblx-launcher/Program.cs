becd32ea9f3750f5f73f071a9341f8eb1e5ee763
ScottBeebiWan <scottbeebiwan@yahoo.com>
2018-09-16 17:46:09 -0700

    absoloutely unfinished!

--- assrblx-launcher/Program.cs
+++ assrblx-launcher/Program.cs
@@ -10,6 +10,25 @@ namespace assrblx_launcher
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("    = = = = = = = = assRBLX Launcher = = = = = = = =");
+            Console.WriteLine("Written in a few minutes by ScottBeebiWan/ScottishScotty");
+            Console.Write("Username: ");
+            string usr = Console.ReadLine();
+            Console.Write("IP: ");
+            string ip = Console.ReadLine();
+            Console.WriteLine("Launching assRBLX...");
+            Cmd("");
+        }
+        static void Cmd(string command)
+        {
+            // Code proudly stolen from stackoverflow.com/questions/1469764
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/C " + command;
+            process.StartInfo = startInfo;
+            process.Start();
         }
     }
 }
